629909b8ccb9c0f0fbdb4e2e57157bc14e387498
DarkGhostHunter <DarkGhostHunter@Gmail.com>
2019-12-08 18:43:21 -0300

    Fixed PHPDoc for helper
    Signed-off-by: DarkGhostHunter <DarkGhostHunter@Gmail.com>

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -8,9 +8,9 @@ if (!function_exists('rut')) {
     /**
      * Returns a Rut instance or null, or the Rut Generator if there are no parameters
      *
-     * @param  mixed ...$rut
-     * @param  null $default
-     * @return \DarkGhostHunter\RutUtils\Rut|\DarkGhostHunter\RutUtils\RutGenerator
+     * @param  null|string|array| $rut
+     * @param  null|mixed $default
+     * @return null|\DarkGhostHunter\RutUtils\Rut|\DarkGhostHunter\RutUtils\RutGenerator
      */
     function rut($rut = null, $default = null)
     {
